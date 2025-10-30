86cf089635095198ee653a2426f37233faf52c94
KKamiyan <carlohilo0000@gmail.com>
2025-10-30 13:43:01 +0800

    data

---
+++ testDataType.java
@@ -0,0 +1,12 @@
+package Transaction;
+
+public class testDataType {
+    
+    boolean flag = false;
+    int number = 0;
+
+    public testDataType(boolean flag, int number){
+        this.flag = flag;
+        this.number = number;
+    }
+}
